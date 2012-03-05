04c2feb9264123c94f022a889c545699b8876f80
Phil Austin <paustin@eos.ubc.ca>
2012-03-05 07:46:16 -0800

    add try/catch for low wv values

--- matlab/thermlib/findTmoist.m
+++ matlab/thermlib/findTmoist.m
@@ -1,11 +1,24 @@
 function theTemp = findTmoist(thetaE0,press);
+   c=constants;
    % get temperatures along a moist adiabat with thetaE0
    %input: thetaE0 (K), press (pa)
    %output: temperature (K) of thetaE0 adiabat at pressure=press
    shortHandle=@(Temp) thetaEchange(Temp,thetaE0,press);
    % this assumes the dewpoint is somewhere between
    % 250-350K
-   theTemp=fzero(shortHandle,[200,400]);
+   try
+      theTemp=fzero(shortHandle,[200,400]);
+   catch
+      %fzero may fail if wv is near 0 g/kg
+      %assume no water vapor and use dry potential
+      %temperature instead
+      theTemp=thetaE0*(press/1.e5)^(c.Rd/c.cpd);
+      %no check to see whether wsat really is small
+      wv=wsat(theTemp,press);
+      if(wv > 1.e-5)
+        fprintf('failed to converge, check limits\n')
+      end
+   end
 end
 
 function result = thetaEchange(Tguess,thetaE0,press);
