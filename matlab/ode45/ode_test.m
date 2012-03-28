04f289ae7aa06c8199181101b8e9d24b52eac008
Phil Austin <paustin@eos.ubc.ca>
2012-03-28 14:02:12 -0700

    ode code

--- matlab/ode45/ode_test.m
+++ matlab/ode45/ode_test.m
@@ -1,9 +1,34 @@
 function ode_test
+  % test prograrm to demonstrate how to
+  % use an event handler to stop an
+  % integration and savethe results
+  % do "help ode45" and scan for EVENTS
+  
   tspan=0:0.01:10;
   [t,y]=ode45(@F,tspan,[2,8]);
   figure(1);
   clf;
   plot(t,y(:,1));
+  title('height vs. time, no stopping');
+
+  stopHeight=5;
+  options=odeset('Events',@events);
+  [t,y]=ode45(@F,tspan,[2,8],options);
+  figure(2);
+  clf;
+  plot(t,y(:,1));
+  title('height vs. time, stop when descending past 5');
+
+  function [value,isterminal,direction] = events(t,y)
+        % Locate the time when height passes through stopHeight in a decreasing direction
+        % and stop integration.  Here we use a nested function to avoid
+        % passing the additional parameter stopHeight as an input argument.
+        value = y(1) - stopHeight;     % detect height = 0
+        isterminal = 1;   % stop the integration
+        direction = -1;   % only when approaching stopHeight from above
+  end
+
+  
 end
 
 function xp=F(t,x)
