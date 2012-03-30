db85eed80082a8df0ea9e75dbd655a092e2ac058
Harry Yau <yauharry@gmail.com>
2012-03-29 18:43:10 -0700

    my working ode code

--- ode/day31.m
+++ ode/day31.m
@@ -14,166 +14,74 @@ function day31
     temp=nc_varget(filename,sound_var,[0,2],[Inf,1]);
     dewpoint=nc_varget(filename,sound_var,[0,3],[Inf,1]);
     height=nc_varget(filename,sound_var,[0,1],[Inf,1]);
-    newheight = nudgeHeight(height);
-%     fh=figure(1);
-%     semilogy(temp,press);
-%     hold on;
-%     semilogy(dewpoint,press);
-%     set(gca,'yscale','log','ydir','reverse');
-%     ylim([400,1000]);
-%     ylabel('press (hPa)');
-%     xlabel('Temp (deg C)');
-%     title('sounding 1');
-%     hold off;
-%     figHandle=figure(2);
-%     skew=30.;
-%     [figHandle,outputws,handlews]=makeSkew(figHandle,skew);
-%     xtemp=convertTempToSkew(temp,press,skew);
-%     xdew=convertTempToSkew(dewpoint,press,skew);
-%     semilogy(xtemp,press,'g-','linewidth',5);
-%     semilogy(xdew,press,'b-','linewidth',5);
-%     %use lowest sounding level for adiabat
-    thetaeVal=thetaes(temp(1) + c.Tc,press(1)*100.);
-    [pressVals,tempVals]=calcAdiabat(press(1)*100.,thetaeVal,400.e2);
-%     xTemp=convertTempToSkew(tempVals - c.Tc,pressVals*1.e-2,skew);
-%     semilogy(xTemp,pressVals*1.e-2,'r-','linewidth',5);
-%     ylim([400,1000.]);
-%     xleft=convertTempToSkew(-20,1.e3,skew);
-%     xright=convertTempToSkew(25.,1.e3,skew);
-%     xlim([xleft,xright]);
-    %
-    % interpolator fails if two pressure values
-    % are the same -- nudge them
-    %
-    newPress=nudgepress(press);
-    interpTenv=@(pVals) interp1(newPress,temp,pVals);
-    interpTdEnv=@(pVals) interp1(newPress,dewpoint,pVals);
-    trytemp=interpTenv(pressVals*1.e-2);
-%     xTemp=convertTempToSkew(trytemp,pressVals*1.e-2,skew);
-%     semilogy(xTemp,pressVals*1.e-2,'b.','markersize',5);
-%     hold off;
-    pressLevs=linspace(100,press(1),100)*1.e2;
-    %
-    % starting integrating from first sounding level
-    %
-    pressLevs=fliplr(pressLevs);
-    TvDiffHandle=@(pVals) calcTvDiff(pVals,thetaeVal,interpTenv,interpTdEnv);
-    for i=1:numel(pressLevs)
-       Tvdiff(i)=TvDiffHandle(pressLevs(i));
-    end
+    newHeight = nudgeHeight(height);
+    interpTenv=@(zVals) interp1(newHeight,temp,zVals);
+    interpTdEnv=@(zVals) interp1(newHeight,dewpoint,zVals);
+    interpPress=@(zVals) interp1(newHeight,press,zVals);
+    p900_level=find(abs(900 - press) < 2.);
+    p800_level=find(abs(800 - press) < 7.);
+    thetaeVal=thetaep(dewpoint(p900_level) + c.Tc,temp(p900_level) + c.Tc,press(p900_level)*100.);
+    height_800=height(p800_level)
+    yinit=[0.5,height_800];
+    tspan=0:10:2500;
+    derivs=@(t,y) F(t,y,thetaeVal,interpTenv,interpTdEnv,interpPress);
     
-    TvDiffHandle=@(pVals) calcTvDiff(pVals,thetaeVal,interpTenv,interpTdEnv);
-    pressLevs=linspace(100,press(1),100)*1.e2;
-    %
-    % starting Buoyancy from first sounding
-    %
-    pressLevs=fliplr(pressLevs);
-    BHandle=@(pVals) calcBuoyancy(pVals,thetaeVal,interpTenv,interpTdEnv);
-    for i=1:numel(pressLevs)
-       B(i)=BHandle(pressLevs(i));
-    end
     
-%     figure(2);
-%     clf;
-%     plot(Tvdiff,pressLevs*0.01,'k-');
-%     set(gca,'ydir','reverse');
-%     ylabel('pressure (hPa)');
-%     xlabel('Virtual temperature difference (K)');
-%     title('Tvdiff vs. pressure');
+    options = odeset('Events', @events);
+    
+    [t,y]=ode45(derivs,tspan,yinit, options);
+    y(end,:) = [] %One negative updraft. 
+                  %That entry pops off which is at the end
+    wvel=y(:,1);
+    height=y(:,2);  
+    %limit the plot to the updraft
+    updraft=wvel > 0;
     figure(1);
     clf;
-    plot(B,pressLevs*0.01,'k-');
-    set(gca,'ydir','reverse');
-    ylabel('pressure (hPa)');
-    xlabel('Buoyancy (m/s^2)');
-    title('Buoyancy vs Pressure');
-    
-    yinit=[0,0];
-    derivs=@(t,y) F(t,y,thetaeVal,interpTenv,interpTdEnv);
-    [t,y]=ode45(derivs,pressLevs,yinit);
-    
-    figure(2);
-    clf;
-    plot(y(:,2),pressLevs*0.01,'k-');
-    set(gca,'ydir','reverse');
-    ylabel('pressure (hPa)');
-    xlabel('w vertical velocity (m/s)');
-    title('W, Vertical Velocity vs. Pressure');
-    
-    pressLevs=linspace(200,press(1),100)*1.e2;
-    %
-    % starting integrating from first sounding level
-    %
-    pressLevs=fliplr(pressLevs);
-    TvDiffHandle=@(pVals) calcTvDiff(pVals,thetaeVal,interpTenv,interpTdEnv);
-    for i=1:numel(pressLevs)
-       Tvdiff(i)=TvDiffHandle(pressLevs(i));
-    end
-    cumCAPE= -c.Rd*cumsum(Tvdiff(2:end).*diff(log(pressLevs)));
-    figure(4);
-    clf;
-    plot( cumCAPE,pressLevs(2:end)*0.01,'k-');
-    title('cumulative CAPE (J/kg) vs. pressure (hPa)');
-    set(gca,'ydir','reverse'); 
-    
-    figure(5);
-    maxvel=sqrt(2.*cumCAPE);
-    plot(maxvel,pressLevs(2:end)*0.01,'k-');
-    title('maximum updraft (m/s) vs. pressure (hPa)');
-    set(gca,'ydir','reverse');
-    derivs;
-  
-    
+    plot(wvel(updraft),height(updraft))
+    xlabel('vertical velocity');
+    ylabel('height above surface');
+    title('wvel vs. height')
+    grid on;
+
+
 end
 
-function yp=F(t,y,theta0,interpTenv,interpTdEnv)
+
+function [value, isterminal, direction] = events(t,y)
+  value = y(1);     %Detects when updraft = zero
+  isterminal = 1;   %Stops the integration
+  direction = -1;   %Negative Direction Only
+end
+
+function yp=F(t,y,thetae0,interpTenv,interpTdEnv,interpPress)
   yp=zeros(2,1); % since output must be a column vector
-  BHandle=@(pVals) calcBuoyancy(pVals,theta0,interpTenv,interpTdEnv);
-  %first variable is w updraft in m/s
-  yp(2)=BHandle(t);
+  %first variable is velocity in m/s
+  yp(1)=calcBuoy(y(2),thetae0,interpTenv,interpTdEnv,interpPress);
   %second variable is height in m
-  yp(1)= yp(2);
+  yp(2)= y(1);
 end
 
-function newPress=nudgepress(pressVec)
-    %if two balloon pressure levels are idential
-    %add a factor of 0.1% to the second one
-    %so interpolation will work
-    newPress=pressVec;
-    hit=find(abs(diff(newPress)) < 1.e-8);
-    newPress(hit+1)=pressVec(hit) + 1.e-3*pressVec(hit);
-end
-
-function TvDiff=calcTvDiff(press,thetae0,interpTenv,interpTdEnv)
-    %calcTvDiff(press,thetae0,interpTenv,interpTdenv)
-    %input: press (Pa), thetae0 (K), plus function handles for T,Td soundings
-    %output: TvDiff (K)
+function Bout=calcBuoy(height,thetae0,interpTenv,interpTdEnv,interpPress)
+    %calcBuoy(height,thetae0,interpTenv,interpTdenv,interpPress)
+    %input: height (m), thetae0 (K), plus function handles for
+    %T,Td, press soundings
+    %output: Bout = buoyant acceleration in m/s^2
     %neglect liquid water loading in the virtual temperature
     c=constants;
-    Tcloud=findTmoist(thetae0,press);
-    wvcloud=wsat(Tcloud,press);
+    press=interpPress(height)*100.;%hPa
+    Tcloud=findTmoist(thetae0,press); %K
+    wvcloud=wsat(Tcloud,press); %kg/kg
     Tvcloud=Tcloud*(1. + c.eps*wvcloud);
-    Tenv=interpTenv(press*1.e-2) + c.Tc;
-    Tdenv=interpTdEnv(press*1.e-2) + c.Tc;
-    wvenv=wsat(Tdenv,press);
+    Tenv=interpTenv(height) + c.Tc;
+    Tdenv=interpTdEnv(height) + c.Tc;
+    wvenv=wsat(Tdenv,press); %kg/kg
     Tvenv=Tenv*(1. + c.eps*wvenv);
     TvDiff=Tvcloud - Tvenv;
+%     fprintf('%10.3f %10.3f %10.3f\n',press*0.01,height,TvDiff);
+    Bout=c.g0*(TvDiff/Tvenv);
 end
 
-
-function B=calcBuoyancy(press,thetae0,interpTenv,interpTdEnv)
-    TvDiffHandle=@(pVals) calcTvDiff(pVals,thetae0,interpTenv,interpTdEnv);
-    Tv_hat = TvDiffHandle(press);
-    
-    c=constants;
-    Tenv=interpTenv(press*1.e-2) + c.Tc;
-    Tdenv=interpTdEnv(press*1.e-2) + c.Tc;
-    wvenv=wsat(Tdenv,press);
-    Tvenv=Tenv*(1. + c.eps*wvenv);
-    Tv_bar = Tvenv;
-    
-    B = 9.8*(Tv_hat/Tv_bar);
-end
 
 function newHeight=nudgeHeight(zVec)
    %if two balloon height levels are idential or descend
